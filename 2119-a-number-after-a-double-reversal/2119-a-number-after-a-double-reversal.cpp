5f5c572dc1fadd0d0264121900c2d13141f3c502
Rafiqul Islam Munna <44343537+MDRafiqul-Islam@users.noreply.github.com>
2022-10-23 00:24:00 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (68.67%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        if(num == 0)
-            return true; 
-                
+        if(num > 0)
+        {
         if(num % 10 == 0)
             return false;
-        
+        }
         return true;
     }
 };
