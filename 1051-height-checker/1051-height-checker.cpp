b66d655ad7d65cecfa2e7cdb78f1a0f7cccee4fc
Rafiqul Islam Munna <44343537+MDRafiqul-Islam@users.noreply.github.com>
2022-10-25 22:16:52 +0600

    Time: 3 ms (76.75%), Space: 8.2 MB (70.92%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -2,18 +2,9 @@ class Solution {
 public:
     int heightChecker(vector<int>& height) {
         vector<int> expected;
-        int i, j, min, c = 0;
+        int i, c = 0;
         expected.assign(height.begin(), height.end());
-        for(i = 0; i < expected.size(); i++){
-            for(j = i + 1; j < expected.size(); j++){
-                if(expected[i] > expected[j])
-                {
-                    min = expected[j];
-                    expected[j] = expected[i];
-                    expected[i] = min;
-                }  
-            }
-        }
+        sort(expected.begin(), expected.end());
         
         for(i = 0; i < height.size(); i++)
         {
